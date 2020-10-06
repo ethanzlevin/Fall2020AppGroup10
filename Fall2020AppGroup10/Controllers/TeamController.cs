d82c9233aa74d3047c7c769df6d0de748e145ffe
nvp0001 <nvp0001@mix.wvu.edu>
2020-10-06 15:11:21 -0400

    Added migration

---
+++ Fall2020AppGroup10/Controllers/TeamController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fall2020AppGroup10.Data;
+using Fall2020AppGroup10.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fall2020AppGroup10.Controllers
+{
+    public class TeamController : Controller
+    {
+        private ApplicationDbContext database;
+
+        public TeamController(ApplicationDbContext dbContext)
+        {
+            this.database = dbContext; 
+        }
+
+        public IActionResult ListAllPlayers()
+        {
+            List<Team> allTeams = database.Team.ToList();
+            
+            return View(allTeams);
+        }
+    }
+}
--- Fall2020AppGroup10/Data/ApplicationDbContext.cs
+++ Fall2020AppGroup10/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Fall2020AppGroup10.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,9 @@ namespace Fall2020AppGroup10.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        //public DbSet<Team> Team {get; set; }
+        public DbSet<Team> Team {get; set; }
 
-        //public DbSet<Player> Player {get; set; } 
+        public DbSet<Player> Player {get; set; } 
         
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
