35bd45c2aa83cd5207bca2eb0a119323c7c44c83
nvp0001 <nvp0001@mix.wvu.edu>
2020-10-11 22:56:33 -0400

    Added data to player and team

--- Fall2020AppGroup10/Data/DbInitializer.cs
+++ Fall2020AppGroup10/Data/DbInitializer.cs
@@ -19,18 +19,40 @@ namespace Fall2020AppGroup10.Data
        
             if (!database.Team.Any())
             {
+                //1
                 Team team = new Team("Lakers", "LA", "West", 12, 1);
                 database.Team.Add(team);
                 database.SaveChanges();
 
+                //2
                 team = new Team("Clippers", "LA", "West", 5, 8);
                 database.Team.Add(team);
                 database.SaveChanges();
 
+                //3
                 team = new Team("Bulls", "Chicago", "East", 2, 11);
                 database.Team.Add(team);
                 database.SaveChanges();
 
+                //4
+                team = new Team("Wizards", "Washington", "East", 2, 11);
+                database.Team.Add(team);
+                database.SaveChanges();
+
+                //4
+                team = new Team("Jazz", "Utah", "East", 7, 6);
+                database.Team.Add(team);
+                database.SaveChanges();
+
+                //5
+                team = new Team("Raptors", "Toronto", "East", 0, 13);
+                database.Team.Add(team);
+                database.SaveChanges();
+
+                //6
+                team = new Team("Spurs", "San Antonio", "West", 13, 0);
+                database.Team.Add(team);
+                database.SaveChanges();
             }
 
             
@@ -46,11 +68,35 @@ namespace Fall2020AppGroup10.Data
                 database.Player.Add(player);
                 database.SaveChanges();
 
+                dateOfBrith = new DateTime(1991, 6, 29);
+
+                player = new Player(2, "Kawhi", "Leonard", dateOfBrith, "F", 2011, 32.74m, 27.1m, 4.9m, 47m);
+                database.Player.Add(player);
+                database.SaveChanges();
+
+                dateOfBrith = new DateTime(1990, 5, 2);
+
+                player = new Player(2, "Paul", "George", dateOfBrith, "F", 2010, 33.01m, 21.5m, 3.9m, 43.9m);
+                database.Player.Add(player);
+                database.SaveChanges();
+
+                dateOfBrith = new DateTime(1989, 8, 7);
+
+                player = new Player(6, "DeMar", "DeRozan", dateOfBrith, "F", 2009, 26.54m, 22.1m, 5.6m, 53.1m);
+                database.Player.Add(player);
+                database.SaveChanges();
+
+                dateOfBrith = new DateTime(1985, 7, 19);
+
+                player = new Player(6, "LaMarcus", "Aldridge", dateOfBrith, "F", 2006, 19.69m, 18.9m, 2.4m, 49.3m);
+                database.Player.Add(player);
+                database.SaveChanges();
+
 
 
             }
 
-            
+
         }
             
     }
