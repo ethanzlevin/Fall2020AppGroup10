24bae93b3591b64ff66d137eb48dc5ae531a3d34
ethan levin <ezl0001@mix.wvu.edu>
2020-10-19 12:26:20 -0400

    Added Teams

--- Fall2020AppGroup10/Data/DbInitializer.cs
+++ Fall2020AppGroup10/Data/DbInitializer.cs
@@ -54,9 +54,110 @@ namespace Fall2020AppGroup10.Data
                 team = new Team("Spurs", "San Antonio", "West", 13, 0);
                 database.Team.Add(team);
                 database.SaveChanges();
-            }
+                //7
+                team = new Team("Hawks", "Atlanta", "East", 10, 3);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //8
+                team = new Team("Celtics", "Boston", "East", 12, 1);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //9
+                team = new Team("Nets", "Brooklyn", "East", 9, 4);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //10
+                team = new Team("Hornets", "Charlotte", "East", 0, 13);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //11
+                team = new Team("Cavaliers", "Cleveland", "East", 1, 12);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //12
+                team = new Team("Mavericks", "Dallas", "West", 12, 1;
+                database.Team.Add(team);
+                database.SaveChanges();
 
+                //13
+                team = new Team("Nuggets", "Denver", "West", 10, 3);
+                database.Team.Add(team);
+                database.SaveChanges();
+
+                //14
+                team = new Team("Pistons", "Detroit", "East", 2, 11);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //15
+                team = new Team("Warriors", "Golden State", "West", 12, 1);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //16
+                team = new Team("Rockets", "Houston", "West", 10, 3);
+                database.Team.Add(team);
+                database.SaveChanges();
+
+                //17
+                team = new Team("Grizzlies", "Memphis", "West", 9, 4);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //18
+                team = new Team("Pacers", "Indiana", "East", 10, 3);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //19
+                team = new Team("Heat", "Miami", "East", 12, 1);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //20
+                team = new Team("Bucks", "Milwaukee", "West", 10, 3);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //21
+                team = new Team("Timberwolves", "Minnesota", "West", 6, 6);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //22
+                team = new Team("Pelicans", "New Orleans", "West", 7, 5);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //23
+                team = new Team("Knicks", "New York", "East", 0, 13);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //24
+                team = new Team("Thunder", "Oklahoma City", "West", 5, 7);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //25
+                team = new Team("Magic", "Orlando", "East", 2, 11);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //26
+                team = new Team("76ers", "Philadelphia", "East", 13, 0);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //27
+                team = new Team("Suns", "Phoenix", "West", 6, 7);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //28
+                team = new Team("Trail Blazers", "Portland", "West", 8, 5);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //29
+                team = new Team("Kings", "Sacramento", "West", 4, 9);
+                database.Team.Add(team);
+                database.SaveChanges();
+                //30
+                team = new Team("Jazz", "Utah", "West", 10, 3);
+                database.Team.Add(team);
+                database.SaveChanges();
+
+            }
             
+
+
             DateTime dateOfBrith = new DateTime(1984, 12, 30);
             decimal salary = 37.2m;
             decimal pointspergame = 25.3m;
