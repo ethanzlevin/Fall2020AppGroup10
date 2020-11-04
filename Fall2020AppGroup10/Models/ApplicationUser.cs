6dcc87d1e62e079df04c4654391cbb30404becfb
nvp0001 <nvp0001@mix.wvu.edu>
2020-11-04 15:55:49 -0500

    added ApplicationUser and user subclass

---
+++ Fall2020AppGroup10/Models/ApplicationUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fall2020AppGroup10.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public ApplicationUser(string firstName, string lastName, string address, string phoneNumber, string email, string password)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+            this.UserName = email;
+            this.PasswordHash = password;
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            passwordHasher.HashPassword(this, password);
+            this.SecurityStamp = Guid.NewGuid().ToString();
+        }
+
+        public ApplicationUser() { }
+
+    }
+}
---
+++ Fall2020AppGroup10/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fall2020AppGroup10.Models
+{
+    public class User : ApplicationUser
+    {
+        //there will be a list here from Bet
+        //public List<Bet> UserBets {get; set; }
+
+        public User(string firstName, string lastName, string address, string phoneNumber, string email, string password) :
+            base(firstName, lastName, address, phoneNumber, email, password)
+        {
+            //this.UserBets = new List<Bet>();
+        }
+
+        public User() { }
+    }
+}
