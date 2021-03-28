523edd98313210face0fd62be84ca7aa45738d44
nvp0001 <nvp0001@mix.wvu.edu>
2021-03-28 13:34:03 -0400

    Added the Registration function with javascript

---
+++ Fall2020SolutionGroup10/Fall2020AppGroup10/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Fall2020AppGroup10.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Fall2020AppGroup10.Areas.Identity.IdentityHostingStartup))]
+namespace Fall2020AppGroup10.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Fall2020SolutionGroup10/Fall2020AppGroup10/wwwroot/js/ModifyViewForUserRole.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(
+
+    function () {
+        $('#numberOfHoursWorkedGroup').hide();
+
+        $('#userRole').change(
+
+            function () {
+
+                var userRole = $('#userRole').val();
+
+                if (userRole == "Employee") {
+                    $('#numberOfVolunteerHoursGroup').show();
+                }
+                else {
+                    $('#numberOfVolunteerHoursGroup').hide();
+                }
+
+            }
+
+        );
+    }
+);
