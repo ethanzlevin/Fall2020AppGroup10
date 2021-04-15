dfd4cda3f6cb46ebf6a6c52c5cec72a1ff92dbb4
nvp0001 <nvp0001@mix.wvu.edu>
2021-04-15 10:44:54 -0400

    Added role assignment function, and view

--- Fall2020SolutionGroup10/Fall2020AppGroup10/Models/ApplicationUserRepo.cs
+++ Fall2020SolutionGroup10/Fall2020AppGroup10/Models/ApplicationUserRepo.cs
@@ -24,5 +24,59 @@ namespace Fall2020AppGroup10.Models
             string userID = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
             return userID;
         }//end FindUserID
+
+        public ApplicationUser FindApplicationUser(string userID)
+        {
+            ApplicationUser applicationUser = database.ApplicationUser.Find(userID);
+
+            return applicationUser;
+        }
+
+        public string GetAvailableRoles(string Id)
+        {
+            string jsonData = null;
+
+            var availableRoleList =
+                 //not in 
+                 from R in database.Roles
+                 where !
+                 (
+                 //in 
+                 from UR in database.UserRoles
+                 where UR.UserId == Id
+                 select UR.RoleId
+                 )
+                 .Contains(R.Id)
+                 select new { R.Id, R.Name };
+
+            jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(availableRoleList);
+
+            return jsonData;
+        }
+
+        //Json
+        public string GetCurrentRoles(string Id)
+        {
+            string jsonData = null;
+            //LINQ -> SQL sql in c# //select from (join) where
+            var userRoleList =
+                from UR in database.UserRoles
+                join R in database.Roles
+                on UR.RoleId equals R.Id
+                where UR.UserId == Id
+                select new { R.Id, R.Name };
+
+            jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(userRoleList);
+
+            return jsonData;
+        }
+
+        public List<ApplicationUser> ListAllApplicationUsers()
+        {
+            List<ApplicationUser> applicationUsers = database.ApplicationUser.ToList<ApplicationUser>();
+
+            return applicationUsers;
+
+        }
     }
 }
---
+++ Fall2020SolutionGroup10/Fall2020AppGroup10/wwwroot/js/GetRolesForUsers.js
@@ -0,0 +1,23 @@
+﻿function GetRolesForUser(dataUrl, checkBoxName, rolesTarget) {
+
+    var listRoles = $(rolesTarget);
+    var usersTarget = $('#usersTarget').val();
+
+    $.getJSON(
+        {
+            url: dataUrl,
+            data: { id: usersTarget },
+            success: function (data) {
+                listRoles.empty();
+                $.each(data, function () {
+                    //listRoles.append('<input type="checkbox" name = "' + checkBoxName + '" value = "' + this.Name + '">' + this.Name + '</br>');
+                    listRoles.append('<option value="' + this.Name + '"> ' + this.Name + ' </option>');
+                }
+                );
+            },
+            error: function () { alert("Data not recieved"); }
+
+        }
+    );
+
+}
